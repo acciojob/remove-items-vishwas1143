437840de418dd683a60d2e9b6aa812bcc29dc99d
vishwas1143 <137210183+vishwas1143@users.noreply.github.com>
2025-03-01 11:10:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let colors = document.getElementById("colorSelect");
+let button =document.getElementById("btn");
+button.addEventListener('click',()=>{
+	colors.remove(colors.selectedIndex);
+})
+
